d1ab4850d9e38fd81c066e4091d173b438f52f09
George Smyrnis <geosmirnis@gmail.com>
2021-06-03 14:51:44 -0500

    Removed some unused parts.

--- denoising_baselines.py
+++ denoising_baselines.py
@@ -9,8 +9,6 @@ from torchvision.datasets import ImageFolder
 from utils import *
 
 from skimage.restoration import denoise_nl_means, estimate_sigma
-from generative_inpainting_pytorch.model.networks import Generator
-from generative_inpainting_pytorch.gen_utils.tools import normalize as gen_normalize
 
 class BaselineDenoiseDistortVal:
     """
@@ -28,16 +26,7 @@ class BaselineDenoiseDistortVal:
                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
             )
 
-        if args.distortion == "randommask":
-            distortion_denoise = RandomMaskGenerativeInpainting(percent_missing=convnoise(args.percent_missing, epoch))
-            self.transform = transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                distortion_denoise,
-                normalize
-            ])
-        elif args.distortion == "gaussiannoise":
+        if args.distortion == "gaussiannoise":
             distortion = GaussianNoise(std=convnoise(args.std, epoch), fixed=args.fixed_mask)
             denoise = NLMeansDenoising()
             self.transform = transforms.Compose([
@@ -71,40 +60,3 @@ class NLMeansDenoising:
             sigma_est = self.sigma_given
         img_denoised = denoise_nl_means(img, sigma=sigma_est, fast_mode=True, multichannel=True)
         return torch.Tensor(img_denoised).to(x.device).permute(2,0,1)
-
-class RandomMaskGenerativeInpainting:
-    """
-    Torchvision transform that tries to perform generative inpainting on an image,
-    which has some of its pixels erased.
-    """
-    def __init__(self, percent_missing):
-        assert isinstance(percent_missing, float) or isinstance(percent_missing, list)
-        self.percent_missing = percent_missing
-        self.netG = Generator({'input_dim': 3, 'ngf': 32}, False, None)
-
-    def __call__(self, image):
-        h, w = image.shape[-2:]
-
-        if isinstance(self.percent_missing, float):
-            removed_num = int(h*w*self.percent_missing)
-        else:
-            removed_percent = np.random.uniform(self.percent_missing[0], self.percent_missing[1])
-            removed_num = int(h*w*removed_percent)
-        removed_secs = np.random.choice(h*w, removed_num, replace=False)
-        mask = torch.ones(h*w)
-        mask[removed_secs] = 0
-        mask = 1.-mask
-        mask = mask.view(h,w)
-        mask = torch.cat(3*[mask.unsqueeze(dim=0)])
-
-        image_norm = gen_normalize(image)
-        print(image_norm.shape)
-        print(mask.shape)
-        image_missing_pixels = image_norm*(1.-mask)
-        image1, image2, _ = self.netG(image_missing_pixels.unsqueeze(dim=0), mask.unsqueeze(dim=0))
-        recon = image2* mask.unsqueeze(dim=0) + image_missing_pixels.unsqueeze(dim=0) * (1. - mask.unsqueeze(dim=0))
-
-        # Return to proper range
-        recon = recon.add_(1.).mul_(0.5)
-
-        return recon.view(h,w)
--- u2nets/__init__.py
+++
@@ -1,3 +0,0 @@
-from .u2net import U2NET
-from .u2net import U2NETP
-from .remove_bg import BGMask
